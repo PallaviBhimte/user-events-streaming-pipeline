ae99134f7fd822ad777e568c2d60ffa3a36c2ec0
PallaviBhimte <p28bhimte@gmail.com>
2026-08-21 12:11:54 +1000

    Edit stream data func

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import logging
 from airflow import DAG
 import json
 import uuid
@@ -39,11 +40,20 @@ def format_data(res):
 
 def stream_data():
     from kafka import KafkaProducer
-    res = get_data()
-    res = format_data(res)
+    
     producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
-    producer.send('users_created', json.dumps(res).encode('utf-8'))
-    producer.flush()
+    curr_time = time.time()
+    
+    while True:
+        if time.time() > curr_time + 60:
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occurred: {e}')
+            continue
 
 with DAG('user_automation',
          default_args=default_args,
